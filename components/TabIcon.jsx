8f5d5bf1cfbba0eaa962a7f337d36c331e6c8c25
Mr. Rafiqul Islam <rafiqulislamemon05@gmail.com>
2025-07-17 00:45:20 +0600

    Feat: Navigation complete with Bottom Tabs

---
+++ components/TabIcon.jsx
@@ -0,0 +1,23 @@
+import { View, Text, Image } from 'react-native'
+import React from 'react'
+
+const TabIcon = ({ icon, color, name, focused }) => {
+  return (
+    <View className="items-center justify-center gap-2">
+      <Image
+        source={icon}
+        resizeMode="contain"
+        tintColor={color}
+        className="w-6 h-6"
+      />
+      <Text
+        className={`${focused ? "font-psemibold" : "font-pregular"} text-xs w-full text-center`}
+        style={{ color: color }}
+      >
+        {name}
+      </Text>
+    </View>
+  );
+};
+
+export default TabIcon

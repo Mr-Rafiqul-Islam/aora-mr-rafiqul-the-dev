55bf6d34c10cf6ca96aa6d73ae18acc70a22cf0c
Mr. Rafiqul Islam <rafiqulislamemon05@gmail.com>
2025-07-16 23:36:56 +0600

    create route

--- app/index.jsx
+++ app/index.jsx
@@ -1,3 +1,4 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
@@ -10,6 +11,7 @@ export default function Index() {
       }}
     >
       <Text className="text-3xl font-pblack">Aora.!</Text>
+      <Link href="/home"> Go to Home</Link>
     </View>
   );
 }
